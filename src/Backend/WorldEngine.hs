1ee8572848fe50a2cd2798dc2a9239d4d9d3b9cc
Dref360 <frederic.branchaud-charron@usherbrooke.ca>
2016-01-30 15:22:17 -0500

    handle command movement

--- src/Backend/WorldEngine.hs
+++ src/Backend/WorldEngine.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
-module WorldEngine(getWorldForJSON, getNewWorld, addPlayer,removePlayer,damageToPlayer,movePlayer, WorldEngine (..)) where
+module WorldEngine(getWorldForJSON, getNewWorld, addPlayer,removePlayer,damageToPlayer,movePlayer, WorldEngine (..),getPlayer,handleControl) where
 import World (World(..))
 import Character (Player(..),Ennemy(..), Character (..))
 import Bullet (Bullet(..),moveBullet)
 import qualified Data.Map.Strict as Map
 import qualified Network.SocketIO as SocketIO
 import qualified Control.Concurrent.STM as STM
+import Linear.V2
 
 data WorldEngine = WorldEngine (Map.Map Int Player) (Map.Map Int Bullet) (Map.Map Int Ennemy) deriving (Eq)
 
@@ -27,4 +28,25 @@ damageToPlayer (WorldEngine players bullet ennemy) uuid dmg =
 movePlayer (WorldEngine players bullet ennemy) uuid position =
     (WorldEngine (Map.adjust (\ p -> move p position) uuid players) bullet ennemy)
 
+
+--Should not use this too often, just a helper function
+getPlayer :: WorldEngine -> Int -> Maybe(Player)
+getPlayer (WorldEngine players bullet ennemy) uuid = Map.lookup uuid players
+
     --TODO CRUD Bullet and Ennemy
+    --TODO get Hit?
+
+mouvement :: String -> V2 Float
+mouvement x = case x of
+               "w"  -> V2   0.0    1.0
+               "a"  -> V2 (-1.0)   0.0
+               "s"  -> V2   0.0  (-1.0)
+               "d"  -> V2   1.0    0.0
+               "wa" -> V2 (-1.0)   1.0
+               "wd" -> V2   1.0    1.0
+               "sa" -> V2 (-1.0) (-1.0)
+               "sd" -> V2   1.0  (-1.0)
+               otherwise -> V2 0.0 0.0
+
+handleControl :: WorldEngine -> String -> Int -> WorldEngine
+handleControl world x uuid = movePlayer world uuid (mouvement x)
--- test/BackendSpec/WorldEngineSpec.hs
+++ test/BackendSpec/WorldEngineSpec.hs
@@ -3,7 +3,7 @@ module BackendSpec.WorldEngineSpec (main, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
-import WorldEngine(getWorldForJSON, getNewWorld, addPlayer,removePlayer,damageToPlayer,movePlayer, WorldEngine (..))
+import WorldEngine(getWorldForJSON, getNewWorld, addPlayer,removePlayer,damageToPlayer,movePlayer, WorldEngine (..),getPlayer,handleControl)
 import World (World(..))
 import Character (Player(..),Ennemy(..), Character (..))
 import Data.Maybe
@@ -23,3 +23,18 @@ spec = describe "WorldEngine" $ do
 
         it "getWorldForJSON player and addPlayer" $ do
           (getWorldForJSON $ addPlayer getNewWorld (Player 0 15 (V2 1.0 1.0) (V2 1.0 1.0))) `shouldBe` (World [(Player 0 15 (V2 1.0 1.0) (V2 1.0 1.0))] [] [] [])
+
+        it "getPlayerTest true" $ do
+          let x = addPlayer getNewWorld (Player 0 15 (V2 1.0 1.0) (V2 1.0 1.0))
+            in
+              (isJust  (getPlayer x 0)) `shouldBe` True
+
+        it "getPlayerTest false" $ do
+          let x = addPlayer getNewWorld (Player 0 15 (V2 1.0 1.0) (V2 1.0 1.0))
+            in
+              (isJust  (getPlayer x 23)) `shouldBe` False
+
+        it "movePlayer" $ do
+          let x = addPlayer getNewWorld (Player 0 15 (V2 1.0 1.0) (V2 1.0 1.0))
+            in
+              position (fromJust  (getPlayer (handleControl x "w" 0) 0)) `shouldBe` (V2 1.0 2.0)
