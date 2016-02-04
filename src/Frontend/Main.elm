77746853ad8964efc574a01f6ed0774d583a21ec
PhilippePaul <philippe.paul@live.com>
2016-02-04 16:32:09 -0500

    Started code to divide map into sections to avoid lag.

--- src/Frontend/Main.elm
+++ src/Frontend/Main.elm
@@ -11,7 +11,7 @@ import Map exposing (..)
 
 
 --display : Signal (Int, Int) -> Signal Bullet -> Signal Player -> Graphics.Collage.Element
-display = Signal.map2 (\(w,h) {player, bullets, field} -> Graphics.Collage.collage w h <| displayMap field ++ displayBullets (w,h) bullets ++ displayPlayer (w,h) player)
+display = Signal.map2 (\(w,h) {player, bullets, field} -> Graphics.Collage.collage w h <| displayMap w h field ++ displayBullets (w,h) bullets ++ displayPlayer (w,h) player)
 
 main =
   display dimensions <| run <| getEvents playerInput mouseInput (fps 30) dimensions Mouse.clicks
--- src/Frontend/Map.elm
+++ src/Frontend/Map.elm
@@ -11,30 +11,44 @@ import Point exposing (..)
 import Math.Vector2 exposing (..)
 
 mapTileList = List.repeat 600 0
-initialMap = { element = flow down (getRows 40 mapTileList), position = Math.Vector2.vec2 0 0 }
+initialMap = { sections = makeSections 40 mapTileList, position = Math.Vector2.vec2 0 0 }
 
-type alias Map = { element : Element, position : Point }
+type alias Map = { sections : List Element, position : Point }
 
 --Association of ints to a certain sprite sheet image
-tileImages : Dict Int (Int, Int)
-tileImages = Dict.fromList (zip [0..1] [(0, 0), (136, 170)])
+crops : Dict Int (Int, Int)
+crops = Dict.fromList (zip [0..1] [(0, 0), (136, 170)])
 
-getCrop : Int -> Element
-getCrop index = croppedImage (Maybe.withDefault (0, 0) (get index tileImages)) 32 32 "../../resources/sheets/tiles.png"
+images : Dict Int Element
+images = Dict.fromList (zip [0..1] (List.map (\index -> (croppedImage (Maybe.withDefault (0, 0) (get index crops)) 32 32 "../../resources/sheets/tiles.png")) [0..1]))
+
+getTile : Int -> Element
+getTile index = Maybe.withDefault (show "Invalid index") (Dict.get index images)
 
 getImages : List Int -> List Element
-getImages tiles = List.map (\index -> collage 64 64 [(Graphics.Collage.scale 2 (toForm (getCrop index)))]) tiles
+getImages = List.map (\index -> (getTile index))
 
 getRows : Int -> List Int -> List Element
 getRows w tiles = case tiles of
                         [] -> []
                         _ -> (flow right (getImages (List.take w tiles)))::(getRows w (List.drop w tiles))
 
+--width of actual map, height of section, list of tiles
+getSectionTiles : Int -> Int -> List Int -> List Int
+getSectionTiles w h tiles =
+  case h of
+    0 -> []
+    _ -> (List.append (List.take 10 tiles) (getSectionTiles w (h - 1) (List.drop w tiles)))
+
+--Suppose a section is 10 x 5 tiles
+makeSections : Int -> List Int -> List Element
+makeSections w tiles = [(flow down (getRows 10 (getSectionTiles w 5 tiles)))]
+
 tickMap : Map -> Point -> Map
 tickMap field direction = { field | position = Math.Vector2.vec2 ((getX field.position) - (getX direction)) ((getY field.position) + (getY direction)) }
 
-displayMap : Map -> List Graphics.Collage.Form
-displayMap field = [toForm (Graphics.Element.container 300 300 (middleAt (absolute (round (getX field.position))) (absolute (round (getY field.position)))) field.element ) ]
+--displayMap : Map -> List Graphics.Collage.Form
+displayMap x y field = [ toForm (Graphics.Element.container 300 300 (middleAt (absolute (round (getX field.position))) (absolute (round (getY field.position)))) (Maybe.withDefault (show "error") (List.head field.sections))) ]
 
 --Utility
 zip : List a -> List b -> List (a,b)
